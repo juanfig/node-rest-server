b89391b72c7c12e9d4c71b15ecafd199549f0013
Juan Figuera <juanrfv@gmail.com>
2019-03-19 21:34:01 -0400

    Variable de Entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -7,7 +7,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://jaguar:ZOoBy0S2Kb6fVRk1@cluster0-mqtf0.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
